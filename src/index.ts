32147adf6b0fd07a86a93c77a25b3722aa535485
Will Melani <william.melani@live.com>
2017-11-09 17:40:20 -1000

    Update packaging

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+
+    export {default as StdOutLogger} from "./StdOutLogger";
+    export {ILoggingEntry} from "./ILoggingEntry";
+    export {ILogger} from "./ILogger";
+    export {IErrorFields, default as ErrorFormatter} from "./utilities/ErrorFormatter";
+    export {default as Stopwatch} from "./utilities/Stopwatch";
+    export {IIncomingMessageLoggingEntry} from "./schemas/messaging/IIncomingMessageLoggingEntry";
+    export {IHttpLoggingEntry} from "./schemas/http/IHttpLoggingEntry";
+    export {IErrorLoggingEntry} from "./schemas/error/IErrorLoggingEntry";
